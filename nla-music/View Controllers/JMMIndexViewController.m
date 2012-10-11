f5df4c04203cc4af20fac0eb1dda015e6e94aec5
Jake MacMullin <jmacmullin@me.com>
2012-10-11 14:15:39 +1100

    Created an initial empty Xcode project.

---
+++ nla-music/View Controllers/JMMIndexViewController.m
@@ -0,0 +1,35 @@
+//
+//  JMMIndexViewController.m
+//  nla-music
+//
+//  Created by Jake MacMullin on 11/10/12.
+//  Copyright (c) 2012 Jake MacMullin. All rights reserved.
+//
+
+#import "JMMIndexViewController.h"
+
+@interface JMMIndexViewController ()
+
+@end
+
+@implementation JMMIndexViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self!=nil) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
